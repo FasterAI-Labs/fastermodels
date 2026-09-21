5160dcb027e10cbf6bb4b60289d2e52f201208cd
nathanhubens <nathan.hubens@gmail.com>
2026-09-21 15:59:08 +0200

    release: 0.1.2

--- fastermodels/__init__.py
+++ fastermodels/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 
 from fastermodels.model import FasterModel, spec_from, state_hash, resolve, load
 from fastermodels.eval import (predictions, correct_vector, wilson, paired_delta, PairedDelta, agreement,
